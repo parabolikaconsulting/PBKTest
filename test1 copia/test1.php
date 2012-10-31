d0d51a6ced0b1e8c78ab82b7fc3eeb50d0eeb484
Andrea <andreaconti.ps@gmail.com>
2012-11-01 00:13:53 +0100

    Another Test

---
+++ test1 copia/test1.php
@@ -0,0 +1,6 @@
+<?
+//test1.php
+include("session.class.php");
+$s = new Session();
+$s->set("userid","27");
+?>
---
+++ test1 copia/test2.php
@@ -0,0 +1,6 @@
+<?
+//test2.php
+include("session.class.php");
+$s = new Session();
+echo $s->get("userid"); //27
+?>
